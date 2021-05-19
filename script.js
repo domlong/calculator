a8659573e684070d39935cd6f64878205fc22869
Dominic Long <dominic.long.au@gmail.com>
2021-05-20 01:29:28 +1000

    Add function for chaining operations
    Calculator functions as desired when chaining operations without
    pressing the equals key. Need to clean up the logic involved with
    setting operands and operator to achieve full functionality.

--- script.js
+++ script.js
@@ -24,3 +24,68 @@ function operate(operator, a, b) {
         default: return 'oopsy'
     }
 }
+
+function addToDisplay() {
+    if(operating) {
+        display.textContent = 0;
+        operating = false;
+    }
+    if(display.textContent == 0) display.textContent = '';
+    display.textContent += this.textContent;
+}
+
+function setOperator(){
+    operating = true;
+    if(memory != ''){
+        evaluate();
+    }
+    currentOperator = this.id;
+    memory = Number(display.textContent);
+}
+
+function refresh() {
+    display.textContent = 0;
+    memory = '';
+    operand = '';
+    console.clear();
+}
+
+/* function updateDisplay(){
+    display.textContent = screen;
+} */
+
+function evaluate() {
+    operand = Number(display.textContent);
+    memory = operate(currentOperator, memory, operand);
+    display.textContent = memory;
+    //currentOperator = '';
+}
+
+function equals() {
+    evaluate();
+}
+function showGoodStuff() {
+    console.log({'memory': memory, 'operator': currentOperator, 'operand': operand, 'clicked': this.textContent});
+}
+
+const display = document.querySelector('#display');
+let memory='';
+let operand;
+let operating = false;
+let currentOperator = '';
+
+digits = document.querySelectorAll('.digit')
+digits.forEach(digit => digit.addEventListener('click',addToDisplay));
+
+operators = document.querySelectorAll('.operator')
+operators.forEach(operator => operator.addEventListener('click',setOperator));
+
+equal = document.querySelector('#equals')
+equal.addEventListener('click', equals);
+
+buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click',showGoodStuff));
+
+
+clear = document.querySelector('#clear');
+clear.addEventListener('click',refresh)
